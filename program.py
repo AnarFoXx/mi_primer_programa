62961e2ccf88b9602f35a0fda45fee007d7da8cb
AnarFoXx <sergio1234222@gmail.com>
2018-01-10 22:21:03 +0100

    añadiendo los deberes

---
+++ program.py
@@ -0,0 +1,56 @@
+number_to_guess = 2
+
+user_number = int(input("adivina un numero del 1 al 10: "))
+
+if number_to_guess == user_number:
+    print("bien")
+else:
+    print("mal")
+
+number_to_guess = 2
+
+user_number = int(input("adivina un numero del 1 al 10: "))
+
+if number_to_guess == user_number:
+    print("bien")
+else:
+    print("mal")
+
+number_to_guess = 2
+
+user_number = int(input("adivina un numero del 1 al 10: "))
+
+if number_to_guess == user_number:
+    print("bien")
+else:
+    print("mal")
+
+number_to_guess = 2
+
+user_number = int(input("adivina un numero del 1 al 10: "))
+
+if number_to_guess == user_number:
+    print("bien")
+else:
+    print("mal")
+number_to_guess = 2
+
+user_number = int(input("adivina un numero del 1 al 10: "))
+
+if number_to_guess == user_number:
+    print("bien")
+else:
+    print("mal")
+number_to_guess = 2
+
+user_number = int(input("adivina un numero del 1 al 10: "))
+
+if number_to_guess == user_number:
+    print("bien")
+else:
+    print("mal")
+number_to_guess = 2
+
+
+
+
